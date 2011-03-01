1573b6292a643d05892e4edf260ca64e8c8ab8e7
Stefano Sanfilippo <stefano.k.sanfilippo@gmail.com>
2011-03-01 18:17:09 +0100

    Fixed small typos on callback template.

--- tpl-query-callback-template.h
+++ tpl-query-callback-template.h
@@ -26,16 +26,8 @@
 
 #include <QtCore/QList>
 
-// TODO to be used like this:
-//
-//     queryCompletedCallback<TplEntry, Message, MessagePrivateData,
-//       tpl_log_manager_get_messages_for_date_finish>
-//          ((GObject*)logmanager, (GAsyncResult*)result, self->messages);
-
 typedef gboolean (*RetrieveResultsFunction)(TplLogManager*, GAsyncResult*,
                                             GList **, GError**);
-
-using namespace Logger;
 
 template <typename QueryResultsType,
           class QueryPrivateDataType,
@@ -50,7 +42,7 @@ void fillPrivateDataListWithQueryResults(GObject *logmanager,
     GError *error = NULL;
 
     if (!TPL_IS_LOG_MANAGER (logmanager)) {
-        throw Error("Query callback returned an invalid TplLogManager object.");
+        throw Logger::Error("Query callback returned an invalid TplLogManager object.");
     }
 
     // Check whether everything went fine, and retrieves data...
@@ -58,18 +50,18 @@ void fillPrivateDataListWithQueryResults(GObject *logmanager,
                                     &gresults, &error);
 
     if (error) {
-        throw Error(error);
+        throw Logger::Error(error);
     }
 
     // This is placed as second, just as a failback. Otherwise it would prevent
     // more detailed exceptions from being thrown...
     if (!success) {
-        throw Error("An async call to TpLogger failed with no specified error!");
+        throw Logger::Error("An async call to TpLogger failed with no specified error!");
     }
 
     // Iterate over the list, cast GObjects and initialises private data
     for (i = gresults; i; i = i->next) {
-        results << QueryPrivateDataType((TypeForQueryResults*)i->data);
+        results << QueryPrivateDataType((QueryResultsType*)i->data);
     }
 
     // Free search results...
