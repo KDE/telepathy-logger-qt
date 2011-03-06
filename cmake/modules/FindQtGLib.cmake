ec93027e4edaf12adc50b1b0d68890326d906183
Stefano Sanfilippo <stefano.k.sanfilippo@gmail.com>
2011-03-06 17:21:14 +0100

    Added cmake stub macros for TpLogger and QtGLib autoconf

---
+++ cmake/modules/FindQtGLib.cmake
@@ -0,0 +1,2 @@
+set (QTGLIB_INCLUDE_DIR /usr/include/QtGStreamer)
+find_library (QTGLIB_LIBRARIES QtGLib-2.0)
---
+++ cmake/modules/FindTelepathyLogger.cmake
@@ -0,0 +1,2 @@
+set (TELEPATHY_LOGGER_INCLUDE_DIR /usr/include/telepathy-logger-0.1)
+find_library (TELEPATHY_LOGGER_LIBRARIES telepathy-logger)
